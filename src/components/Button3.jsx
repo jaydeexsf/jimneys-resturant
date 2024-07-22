30252162342caa8749b68fc576ec2da64ea02ca5
johannes <jaydeexsf@gmail.com>
2024-07-22 04:35:48 +0200

    04:35

--- src/components/Button3.jsx
+++ src/components/Button3.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
-// import useState from 'react';
+import React, { useState } from 'react';
 
- const Button3 = ({cont}) => {
-  // const [category, setCategory] = useState("All");
+const Button3 = ({ cont }) => {
+  const [category, setCategory] = useState("All");
 
-  const handleClick = ()=> {
-    // setCategory(cont)
-      console.log(cont);
-  }
+  // const handleClick = () => {
+  //     console.log(cont);
+  // }
 
-//  function gg() {
-//  console.log(HandleClick)
-//  }
+  // setCategory(); // This line should be removed
+
+  // function gg() {
+  // console.log(HandleClick);
+  // }
 
   return (
     <div>
-        <button onClick={handleClick} className="btn w-auto px-[15px] bg-gray-300 text-black text-xs border flex justify-center items-center rounded-[5px] h-[33px]">{cont}</button>
+      <button
+        onClick={() => {
+          setCategory(cont);
+        }}
+        className="btn w-auto px-[15px] bg-gray-300 text-black text-xs border flex justify-center items-center rounded-[5px] h-[33px]">
+        {cont}
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Button3;
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,47 +1,44 @@
 import React from 'react';
-// import products from '../database/index';
+import products from '../database/index';
 // import { useState } from 'react';
-// // import { FaDisplay } from 'react-icons/fa6';
-// import MenuItem from './MenuItem';
-// import { Button3 } from './Button3';
+// import { FaDisplay } from 'react-icons/fa6';
+import MenuItem from './MenuItem';
 import Button3 from './Button3';
 import './Filter.css';
 
 export const Filter = () => {
     // const [category, setCategory] = useState('All')
 
-//     let FilteredProducts = products;
+    // let FilteredProducts = products;
 
-//     const FilteredData = products.filter((item) => {
-//         return item.category.toLowerCase() === category.toLowerCase();
-//     });
-    
-    
+    // const FilteredData = products.filter((item) => {
+    //     return item.category.toLowerCase() === category.toLowerCase();
+    // });
 
-  //  const displayAll = 
-  //   products.map((item)=> {
-  //       <MenuItem key={index} item={item} />
-  //   })
+    // const displayAll = 
+    // ));
 
-//   setCategory(HandleClick)
+    // setCategory(HandleClick)
 
-// const displayAll = products.map((item)=> {
-//   return (
-//     <MenuItem key={item.id} item={item} />
-//   )
-// })
+    // const displayAll = products.map((item)=> {
+    //   return (
+    //     <MenuItem key={item.id} item={item} />
+    //   )
+    // })
 
-
-  return (
-    <div className='bg-black h-screen text-white'>
-        <h1 className='flex justify-center'></h1>
-        <div className="flex gap-4 p-[15px]">
-            <Button3 cont={"All"}/>
-            <Button3 cont={"BreakFast"} />
-            <Button3 cont={"Lunch"} />
-            <Button3 cont={'Dinner'} />
+    return (
+        <div className='bg-gray-900 flex flex-col justify-cente items-center h-full text-white'>
+            <h1 className='flex justify-center'></h1>
+            <div className="flex gap-4 mt-[30px] p-[15px]">
+                <Button3 cont={"All"} />
+                <Button3 cont={"BreakFast"} />
+                <Button3 cont={"Lunch"} />
+                <Button3 cont={'Dinner'} />
+            </div>
+            <div className="grid justify-center  w-[100vw] p-[80px] grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                {products.map((item, index) => (
+                 <MenuItem key={index} item={item} />))}
+            </div>
         </div>
-        {/* {category == "All" ? displayAll : <p>What</p>} */}
-    </div>
-  )
-}
+    );
+};
