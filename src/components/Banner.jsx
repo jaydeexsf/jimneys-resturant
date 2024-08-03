ac951b51b61782e7b24ecfd782288643ffa09be9
johannes <mojohannes06@gmail.com>
2024-08-03 17:55:49 +0200

    17:55

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import data from '../database/index';
 // import { Contact } from './Contact';
 // import Contact from '../components/Contact';
 import { Button } from './Button';
 import { Button2 } from './Button2';
+import { Button4 } from './Button4';
 
 const Banner = () => {
 
@@ -16,9 +17,32 @@ const Banner = () => {
     backgroundColor: 'black',
   };
 
+  const bimstyle = {
+    background: `url(${data[4].imageUrl})`,
+    backgroundSize: '',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor: 'black',
+  }
+
+  // const [InnerWidth, setInnerWidth] = useState('');
+
+  // const 
+
+  const [Swindow, setSwindow] = useState(window.innerWidth);
+
+  function seet() {
+    window.addEventListener('resize', () => {
+      setSwindow(window.innerWidth);
+    })
+  }
+
+  seet();
+  console.log(Swindow);
+
   return (
-    <section style={backgroundImageStyle } className="bg-black flex flex-start w-[100%] h-[75vh] text-white py-20">
-      <div className="container 2xl:flex 2xl:justify-center 2xl:w-[100%] items-center 2xl:flex-col w-[40vw] mx-[5vw]">
+    <section style={Swindow < 1535 ? backgroundImageStyle : bimstyle} className="bg-black flex flex-start w-[100%] h-[75vh] text-white py-20">
+      <div className="container 2xl:flex 2xl:justify-center xl:mb-[300px] 2xl:w-[100%] items-center 2xl:flex-col w-[40vw] mx-[5vw]">
         <h1 className="text-[36px] font-medium mb-4">Freshness in <span className="text-red-700">Every</span> Bite</h1>
         <p className="mb-8 text-sm 2xl:w-[50vw] 2xl:text-center">
         At Jimney's Fish & Chips, we bring you a delightful harmony of timeless flavors infused with contemporary culinary flair. Nestled conveniently in the vibrant heart of Mankweng, adjacent to the University of Limpopo and just a stone's throw from the local garage, our restaurant stands as a beacon of exceptional taste and warm hospitality.
@@ -30,12 +54,13 @@ const Banner = () => {
         <Link to="/menu">
             <button className="bg-transparent border hover:text-red-700 ml-[20px] text-white py-2 px-4 rounded-sm">View menu</button>
         </Link> */}
-        <Button fist={"Order Now"} second={"Thank Us Later"}/>
+         {Swindow < 1535 ? <Button fist={"Order Now"} second={"Thank Us Later"} /> : <Button4 fist={'Order now'} second={"Thank Us Later"}/>}
         <Button2 fist={"View Menu"}/>
         </div>
       </div>
     </section>
   );
+  
 };
 
 export default Banner;
---
+++ src/components/Button4.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+export const Button4 = ({fist, second}) => {
+  return (
+    <div>
+        <button className="
+            group
+            p-[22px]
+            hover:bg-black
+            hover:text-red-400
+            cursor-pointer 
+            relative
+            text-sm 
+            font-normal 
+            border-0 
+            flex
+            items-center 
+            justify-center
+            bg-red-500
+            text-gray-900 
+            h-auto  
+            w-[120px]  
+            overflow-hidden   
+            transition-all
+            duration-100">
+            <span class="
+            group-hover:w-full
+            absolute 
+            left-0 
+            h-full 
+            w-5 
+            border-y
+            border-l
+            border-red-500
+                transition-all
+            duration-500">
+            </span>
+
+            <p class="group-hover:opacity-0 group-hover:translate-x-[100%]  translate-x-0 absolute transition-all
+            duration-5000">{fist}</p>
+
+            <span class="group-hover:translate-x-0  group-hover:opacity-100 absolute  translate-x-full opacity-0  transition-all duration-2000">
+                {second}
+            </span>
+
+            <span
+                class="group-hover:w-full absolute right-0 h-full w-5  border-y border-r  border-red-500 transition-all duration-500">
+            </span>
+        </button>
+    </div>
+  )
+}
