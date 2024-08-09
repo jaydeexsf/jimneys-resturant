4f44c802219440622c34125b3ecbfd036b33c7c0
johannes <mojohannes06@gmail.com>
2024-08-09 18:31:25 +0200

    2024/08/09

--- src/components/Button3.jsx
+++ src/components/Button3.jsx
@@ -1,29 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Button3 = ({ cont }) => {
-  const [category, setCategory] = useState("All");
-
-  // const handleClick = () => {
-  //     console.log(cont);
-  // }
-
-  // setCategory(); // This line should be removed
-
-  // function gg() {
-  // console.log(HandleClick);
-  // }
-
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setCategory(cont);
-        }}
-        className="btn w-auto px-[15px] bg-gray-300 text-black text-xs border flex justify-center items-center rounded-[5px] h-[33px]">
-        {cont}
-      </button>
-    </div>
-  );
+const Button3 = ({ cont, onClick, style }) => {
+    return (
+        <button
+            onClick={onClick}
+            className="btn w-auto px-[15px] bg-gray-300 text-black text-xs border flex justify-center items-center rounded-[5px] h-[33px]">
+            {cont}
+        </button>
+    );
 };
 
 export default Button3;
+
