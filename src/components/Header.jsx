c589bdbb97281fdb19f8b4ae468d0e5f69701b88
johannes <mojohannes06@gmail.com>
2024-09-07 14:35:51 +0200

    dd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { CiMenuBurger } from "react-icons/ci";
+import logo from '../assets/images/PSX_20240717_161845-removebg-preview.png'
 
 
 const Header = ({className}) => {
@@ -23,7 +24,7 @@ const style = 'text-red-700 underline decoration-white	decoration-4	underline-of
     <header className={`bg-black p-4`}>
       <div className={`container mx-auto flex justify-between items-center`}>
         <h1 className="text-white items-center text-3xl flex gap-2">
-          <img src='src/assets/images/PSX_20240717_161845-removebg-preview.png' className='w-8' alt="Logo" />
+          <img src={logo} className='w-10' alt="Logo" />
           Jimney's
         </h1>
         <CiMenuBurger onClick={showmenu} className='text-gray-200 text-3xl md:hidden hover:cursor-pointer flex'/>
