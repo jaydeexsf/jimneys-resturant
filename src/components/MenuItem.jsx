3e5f4f737df56fff1298bea4b08c87a6d9e9df47
johannes <mojohannes06@gmail.com>
2024-10-30 22:21:41 +0200

    Loader Added

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -18,7 +18,7 @@ const MenuItem = ({ item, description }) => {
       <h3 className="text-md font-bold text-center">{item.name}</h3>
       <img src={item.imageUrl} alt={item.name} className="w-full h-[150px] object-cover" />
       <div className='bg-white-500 flex-end'>
-      {description ? <p style={price} className="text-white text-[15px] font- px-[4px] rounded-[2px]">R {item.price}</p> : ''}
+      {description ? <p style={price} className="text-white text-[15px] font- px-[4px] rounded-[2px]">R {item.originalPrice}</p> : ''}
       {description ? <p className="text-gray-600 text-xs mb-">{item.description}</p> : ''}
       </div>
     </div>
