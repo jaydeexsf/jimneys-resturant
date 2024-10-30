6801f69ff0ac2d7f6529dff0665d93adc4d3d440
johannes <mojohannes06@gmail.com>
2024-10-30 18:10:41 +0200

    Small Updates

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -7,8 +7,6 @@ import { MdCancel } from "react-icons/md";
 import { IoMdCheckmark } from "react-icons/io";
 import { Link } from 'react-router-dom';
 
-
-
 const AddProduct = () => {
     const [name, setName] = useState('');
     const [imageFile, setImageFile] = useState(null);
@@ -36,9 +34,11 @@ const AddProduct = () => {
         setError(''); 
 
         if(category === '') {
-            setError('Select Category')
+            setError('Select Category');
+            setLoading(false);
+            return;
         }
- 
+
         try {
             const imageRef = ref(storage, `productImages/${imageFile.name}`);
             await uploadBytes(imageRef, imageFile);
@@ -55,7 +55,7 @@ const AddProduct = () => {
             };
 
             await addDoc(collection(db, "products"), newProduct);
-            setMessage('Product added sucesfully')
+            setMessage('Product added successfully');
             console.log('New product added:', newProduct);
             resetForm(); 
         } catch (err) {
@@ -80,30 +80,39 @@ const AddProduct = () => {
     useEffect(()=>{
         if(message){
             setTimeout(() => {
-                setMessage('')
+                setMessage('');
             }, 4000);
         }
-    }, [message])
+    }, [message]);
 
     return (
         <div className="bg-gray-900 min-h-screen flex flex-col items-center text-white p-8">
             <div className='fixed top-[90px] ml-8 left-[50%] z-[1000000] w-full translate-x-[-50%]'>
                 {message ? 
                 <div className='relative w-fit'>
-                    <div className='bg-red-600/90 flex gap-1 items-center pr-8 pl-2 py-2 rounded text-white text-md font-' > <span > <IoMdCheckmark size={20} className="text-green-600 font-bold" />
-                    </span> <span className='text-green-200'>{message}</span> <span className='absolute top-0 right-1'><button onClick={()=> setMessage('')}><MdCancel   size={20}/>
-</button></span> </div>
-                    
+                    <div className='bg-red-600/90 flex gap-1 items-center pr-8 pl-2 py-2 rounded text-white text-md' > 
+                        <span>
+                            <IoMdCheckmark size={20} className="text-green-600 font-bold" />
+                        </span> 
+                        <span className='text-green-200'>{message}</span> 
+                        <span className='absolute top-0 right-1'>
+                            <button onClick={()=> setMessage('')}>
+                                <MdCancel size={20}/>
+                            </button>
+                        </span> 
+                    </div> 
                 </div>    
-            :
-            ''
-            }
-
+            : '' }
             </div>
-            <div className='absolute top-[120px] left-[2%] bg-red-600 flex justify-center items-center px-4 py-1 rounded-full'><Link to="/jimneys-resturant/admin">Back</Link></div>
-            <h1 className="text-xl md:text-2xl font-bold mb-6">Add New Product </h1>
-            <form onSubmit={handleAddProduct} className="bg-gray-800 p-3 text-sm rounded-md w-full max-w-md">
-                {error && <p className="text-red-500 mb-4">{error}</p>} {/* Display error message */}
+
+            <div className='absolute top-[120px] left-[2%] bg-red-600 flex justify-center items-center px-4 py-1 rounded-full'>
+                <Link to="/jimneys-resturant/admin">Back</Link>
+            </div>
+
+            <h1 className="text-xl md:text-2xl font-bold mb-6">Add New Product</h1>
+            <form onSubmit={handleAddProduct} className="bg-gray-800 p-6 text-sm rounded-md w-full max-w-md space-y-4">
+                {error && <p className="text-red-500 mb-2">{error}</p>}
+                
                 <input
                     type="text"
                     placeholder="Product Name"
@@ -111,16 +120,18 @@ const AddProduct = () => {
                     onChange={(e) => setName(e.target.value)}
                     required
                     disabled={loading}
-                    className="border border-gray-700 focus:border-red-600 focus:border-[1.5px] outline-none bg-gray-900 text-white rounded-md p-2 mb-2 w-full"
+                    className="border border-gray-700 focus:border-red-600 outline-none bg-gray-900 text-white rounded-md p-3 w-full"
                 />
+
                 <textarea
                     placeholder="Description"
                     value={description}
                     onChange={(e) => setDescription(e.target.value)}
                     required
                     disabled={loading}
-                    className="border border-gray-700 bg-gray-900 focus:border-red-600 focus:border-[1.5px] outline-none text-white rounded-md p-2 mb-2 w-full"
+                    className="border border-gray-700 bg-gray-900 focus:border-red-600 outline-none text-white rounded-md p-3 w-full h-24"
                 />
+
                 <input
                     type="number"
                     placeholder="Original Price"
@@ -128,29 +139,31 @@ const AddProduct = () => {
                     onChange={(e) => setOriginalPrice(e.target.value)}
                     required
                     disabled={loading}
-                    className="border border-gray-700 bg-gray-900 focus:border-red-600 focus:border-[1.5px] outline-none text-white rounded-md p-2 mb-2 w-full"
+                    className="border border-gray-700 bg-gray-900 focus:border-red-600 outline-none text-white rounded-md p-3 w-full"
                 />
+
                 <input
                     type="number"
                     placeholder="Current Price (optional)"
                     value={currentPrice}
                     onChange={(e) => setCurrentPrice(e.target.value)}
                     disabled={loading}
-                    className="border border-gray-700 bg-gray-900 focus:border-red-600 focus:border-[1.5px] outline-none text-white rounded-md p-2 mb-2 w-full"
+                    className="border border-gray-700 bg-gray-900 focus:border-red-600 outline-none text-white rounded-md p-3 w-full"
                 />
+
                 <input
                     type="text"
                     placeholder="Discount Percentage (optional)"
                     value={discountPercentage}
-                    disabled={loading}
                     onChange={(e) => setDiscountPercentage(e.target.value)}
-                    className="border border-gray-700 bg-gray-900 focus:border-red-600 focus:border-[1.5px] outline-none text-white rounded-md p-2 mb-2 w-full"
+                    disabled={loading}
+                    className="border border-gray-700 bg-gray-900 focus:border-red-600 outline-none text-white rounded-md p-3 w-full"
                 />
-               {/* <label className='text-gray-400'>Category:</label> */}
+
                 <select
                     onChange={(e) => setCategory(e.target.value)}
                     required
-                    className='w-full mb-4 p-2 bg-gray-700 outline-none focus:border-red-600 focus:border-[1.5px] focus:border-sky-600 border-transparent border-[1px] rounded-md text-white'
+                    className='w-full mb-4 p-2 bg-gray-700 outline-none focus:border-red-600 focus:border-[1.5px] border-transparent border-[1px] rounded-md text-white'
                 >
                     <option value="" >Select a category</option>
                     <option value="Lunch">Lunch</option>
@@ -161,24 +174,25 @@ const AddProduct = () => {
                 <div className="mt-4">
                     <label className="text-gray-400 text-sm">Upload Product Image</label>
                     <input
-                        disabled={loading}
                         type="file"
                         accept="image/*"
                         onChange={handleImageChange}
-                        className="block text-gray-400 text-sm file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-red-600 file:hover:cursor-pointer file:text-white hover:file:bg-red-700 mt-2 w-full"
+                        disabled={loading}
+                        className="block w-full mt-2 text-sm text-gray-400 file:py-2 file:px-4 file:rounded-md file:bg-red-600 file:text-white file:font-semibold file:border-0 file:cursor-pointer hover:file:bg-red-700"
                     />
                     {previewImage && (
                         <img src={previewImage} alt="Preview" className="mt-4 w-full h-48 object-cover rounded-md" />
                     )}
                 </div>
+
                 <button
                     type="submit"
                     disabled={loading}
-                    className={`mt-6 w-full flex items-center justify-center gap-2 bg-red-600 px-4 py-2 rounded-md text-white font-semibold ${
+                    className={`mt-6 w-full flex items-center justify-center gap-2 bg-red-600 px-4 py-3 rounded-md text-white font-semibold ${
                         loading ? 'opacity-50 cursor-not-allowed' : 'hover:bg-red-700'
                     }`}
                 >
-                    {loading ?  <div className="border-gray-500 border-t-black border-2 rounded-full w-4 h-4 animate-spin"></div> : ''} 
+                    {loading && <div className="border-t-transparent border-gray-400 border-4 rounded-full w-5 h-5 animate-spin"></div>}
                     {loading ? 'Adding...' : 'Add Product'}
                 </button>
             </form>
